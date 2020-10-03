6ea1b6eb04350134f4b31be1ab143831eb17adb2
digitalparesh <prshnandaniya@gmail.com>
2020-10-03 22:26:47 +0530

    add build folder

---
+++ build/precache-manifest.b393672c172505b5a4f65f3e6b318251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddac4ce57cc6a6859c2f811264c9cf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e839f29cdd219a68a5ab",
+    "url": "/static/css/main.02fd7961.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e839f29cdd219a68a5ab",
+    "url": "/static/js/main.2c9d300c.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "33bd568adf675fd887c55d99f7e2853c",
+    "url": "/static/media/boy-1.33bd568a.jpg"
+  }
+]);
